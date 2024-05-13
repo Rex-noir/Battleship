99f57119a56e6966c2f3fd69b983ca6f8f92a36b
Lala <ace.rexnoir@gmail.com>
2024-05-13 17:28:58 +0530

    Support classList adding with multiple classes at once

--- src/layout/CreateElement.ts
+++ src/layout/CreateElement.ts
@@ -1,11 +1,19 @@
 export default class CreateElements<T extends HTMLElement> {
   private element: T;
-  constructor(tag: string, id?: string, className?: string) {
+  constructor(tag: string, id?: string, classList?: string) {
     this.element = document.createElement(tag) as T;
     if (id) this.element.id = id;
-    if (className) this.element.classList.add(className);
+    if (classList) {
+      const classNames = classList.split(" ");
+      classNames.forEach((name) => {
+        this.element.classList.add(name);
+      });
+    }
   }
   getElement(): T {
     return this.element;
+  }
+  setAttribute(name: string, value: string): void {
+    this.element.setAttribute(name, value);
   }
 }
