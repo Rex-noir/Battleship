aff5ba9a1c25e647f02c72e6824ac00b91d8fa41
AceReX <ace.rexnoir@gmail.com>
2024-05-13 09:59:02 +0530

    Finished initial class structure

---
+++ src/Utils/Ship.ts
@@ -0,0 +1,23 @@
+export default class Ship {
+  private readonly length: number;
+  private sunk: boolean;
+  private hits: number;
+
+  constructor(length: number) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+  hit(): void {
+    this.hits++;
+    if (this.hits > this.length) {
+      this.sunk = true;
+    }
+  }
+  isSunk(): boolean {
+    return this.sunk;
+  }
+  get Length(): number {
+    return this.length;
+  }
+}
