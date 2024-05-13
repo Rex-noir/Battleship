c44afe7987d5e0e906389c17c4e97d12c9668369
Lala <ace.rexnoir@gmail.com>
2024-05-13 17:24:57 +0530

    Add byClass method

--- src/layout/DOMReferencer.ts
+++ src/layout/DOMReferencer.ts
@@ -2,4 +2,7 @@ export default class Reference {
   static byId<T>(id: string): T | null {
     return document.getElementById(id) as T;
   }
+  static byClass<T>(name: string): T | null {
+    return document.querySelector(name) as T;
+  }
 }
