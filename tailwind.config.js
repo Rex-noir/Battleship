f9af984f21789de157eaade46001791012a059d7
Lala <ace.rexnoir@gmail.com>
2024-05-16 09:13:35 +0530

    Add fonts from Google Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      jersey20: ['"Jersey 20 Charted", sans-serif'],
+      sedanSC: ['"Sedan SC", serif'],
+      protestRiotRegular: ['"Protest Riot", sans-serif'],
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+      addVariant("child-active", "&>*:active");
+    },
+  ],
 };
